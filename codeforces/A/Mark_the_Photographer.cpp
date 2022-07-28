72f89bedbd168417540e1bed92e6d0ef4688c7cd
Youssef Badaoui <ybadaoui@e3r1p3.1337.ma>
2022-07-28 16:46:41 +0100

    solving Mark the Photographer probleme

---
+++ codeforces/A/Mark_the_Photographer.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/problemset/problem/1705/A
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    long long  t,n,x,i;cin>>t;
+    while(t--)
+    {
+        i = 0;
+        cin >> n >> x;
+        vector<long>v(n*2);
+        while(i < n*2)
+            cin >> v[i++];
+        sort(v.begin(), v.end());
+        i = 0;
+        int p = 0;
+        while(i < n)
+        {
+            if(v[n+i] - v[i]< x)
+            {
+                cout <<"NO"<<endl;
+                p = 1;
+                break;
+            }
+            i++;
+        }
+        if(!p)
+            cout << "YES" << endl;
+    }
+}
