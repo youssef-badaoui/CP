8f071f4d99c6ac6f4e7d0607e8fd640abfb1b814
Youssef Badaoui <badaouiyoussef8888@gmail.com>
2022-11-01 21:43:33 +0100

    number spiral solved back again

--- CSES/Introductory_Problems/Number_Spiral.cpp
+++ CSES/Introductory_Problems/Number_Spiral.cpp
@@ -1,4 +1,4 @@
-#include <iostrem>
+#include <iostream>
 
 using namespace std;
 
@@ -7,7 +7,29 @@ int main()
     long long t;cin >> t;
     while(t--)
     {
-        long long x,y;cin >> x>>y;
-        
+        long long res = 0;
+        long long x,y; cin >> y >> x;
+
+        if(x >= y)
+        {
+            if(x % 2)
+                res = x * x - (y - 1);
+            else
+            {
+                x--;
+                res = (x * x + 1) + (y - 1);
+            }
+        }
+        else
+        {
+            if(!(y%2))
+                res = y * y - (x - 1);
+            else
+            {
+                y--;
+                res = (y * y + 1) + (x - 1);
+            }
+        }
+        cout << res << endl;
     }
 }
